9c4375b1e93fb83482de42a10760a02bfa516606
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-01-01 20:17:53 -0800

    change noteTableState name

--- src/components/synth/store/chordFamily.tsx
+++ src/components/synth/store/chordFamily.tsx
@@ -1,12 +1,12 @@
 import { atomFamily, selector } from "recoil";
-import notesAtom from "./notesAtom";
+import noteTableState from "./notesAtom";
 
 const chordFamily = atomFamily({
   key: `chordFamily${Date.now()}`,
   default: selector({
     key: `chordFamilyDefault{Date.now()}`,
     get: ({ get }) => {
-      const notes = get(notesAtom);
+      const notes = get(noteTableState);
       const chord = notes.map((note) => ({ note: note, active: false }));
       return chord;
     },
