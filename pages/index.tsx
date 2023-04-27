3130501c35293a82f7bc21ec635cac1d933dd869
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-04-27 14:59:41 -0700

    add footer and links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { Suspense } from "react";
 
 import dynamic from "next/dynamic";
@@ -12,14 +13,31 @@ export default function Home() {
       <Head>
         <title>Synth App</title>
       </Head>
-      <div className="flex justify-around">
-        <h1 className="font-sharetech text-4xl text-emerald">Sequencer</h1>
+      <div className="flex flex-row justify-center w-full pt-4 ">
+        <div className="flex self-center justify-center w-full text-4xl flex-3 font-sharetech text-emerald">
+          <h1>Sequencer</h1>
+        </div>
       </div>
       <div className="p-2">
         <Suspense>
           <Sequencer />
         </Suspense>
       </div>
+      <footer className="flex flex-col items-center w-full h-full pt-8">
+        <p className="self-center text-gray-300">
+          &#169; <a href="https://raffaelecataldo.com">Raffaele Cataldo</a> 2023
+        </p>
+        <div className="self-center p-2 w-18 h-18 flex-end">
+          <a href="https://github.com/farmeroy/synth-app">
+            <Image
+              alt="Link to Github"
+              width={28}
+              height={28}
+              src="/github-mark-white.svg"
+            />
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
