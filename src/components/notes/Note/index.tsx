a1a6329fbff4dc8d2142f934b73d126ee57aa593
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-05-10 10:51:47 -0700

    remove unnecessary noteRowActiveBeatsState atom

--- src/components/notes/Note/index.tsx
+++ src/components/notes/Note/index.tsx
@@ -1,5 +1,5 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import noteRowActiveBeatsAtom from "../../../lib/store/noteRowActiveBeatsAtom";
+import { useRecoilValue } from "recoil";
+import { useState } from "react";
 import { Loop, PolySynth } from "tone";
 import noteIsActive from "../../../lib/store/noteIsActive";
 import { useEffect } from "react";
@@ -15,15 +15,7 @@ export interface NoteProps {
 }
 
 const Note = ({ synth, note, index }: NoteProps) => {
-  const [activeNotes, setActiveNotes] = useRecoilState(
-    // try to replace this atom with
-    // a single atom representing this notes state
-    // -> change noteIsActive to take two coordinates
-    // its own index and the row index {noteIndex: x, rowIndex: y}
-    // then use a selector to collect all the notes and create
-    // an array of active note outside of this component
-    noteRowActiveBeatsAtom(note.note)
-  );
+  const [isPlaying, setIsPlaying] = useState(false);
   const noteIsActiveState = useRecoilValue(noteIsActive(index));
   const machineIsOnState = useRecoilValue(machineIsOnAtom);
   const machineBeatsCountState = useRecoilValue(machineBeatsCount);
@@ -32,7 +24,7 @@ const Note = ({ synth, note, index }: NoteProps) => {
     const loop = new Loop(() => {
       synth.triggerAttackRelease(note.frequency, "8n");
     }, "1n");
-    if (activeNotes[index] === true && machineIsOnState) {
+    if (isPlaying === true && machineIsOnState) {
       loop.start(`0:${index}:0`);
     } else {
       loop.stop().cancel();
@@ -40,19 +32,10 @@ const Note = ({ synth, note, index }: NoteProps) => {
     return () => {
       loop.dispose();
     };
-  }, [
-    activeNotes,
-    synth,
-    index,
-    note,
-    machineIsOnState,
-    machineBeatsCountState,
-  ]);
+  }, [isPlaying, synth, index, note, machineIsOnState, machineBeatsCountState]);
 
   const handleUpdateIsActive = () => {
-    const state = [...activeNotes];
-    state[index] = !activeNotes[index];
-    setActiveNotes([...state]);
+    setIsPlaying((state) => !state);
   };
 
   const styleActive = "bg-violetlight";
@@ -62,7 +45,7 @@ const Note = ({ synth, note, index }: NoteProps) => {
   return (
     <button
       className={`border rounded-lg border-black hover:brightness-150 transition-all  m-px w-full ${
-        activeNotes[index] ? styleActive : styleInactive
+        isPlaying ? styleActive : styleInactive
       } ${noteIsActiveState ? styleCurrentBeat : null}`}
       onClick={handleUpdateIsActive}
     />
