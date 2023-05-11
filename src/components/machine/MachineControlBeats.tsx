d32aa3e723ade9ecbc9aae048a89d9fa9cc5310b
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-05-11 11:46:16 -0700

    remove console.log

--- src/components/machine/MachineControlBeats.tsx
+++ src/components/machine/MachineControlBeats.tsx
@@ -1,10 +1,11 @@
 import machineBeatsCount from "../../lib/store/machineBeatsCount";
 import { useRecoilState } from "recoil";
 import { CircleMinus, CirclePlus } from "tabler-icons-react";
+
 const MachineControlBeats = () => {
   const [machineBeatsCountState, setMachineBeatsCountState] =
     useRecoilState(machineBeatsCount);
-  console.log(machineBeatsCountState);
+
   return (
     <div className="flex w-full p-3 m-1 border rounded-full border-violetlight hover:border-emerald bg-violetlight">
       <label htmlFor="beat" className="mx-2 text-emerald">
