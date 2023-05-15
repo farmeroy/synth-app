88d32469f03c5d2f0bace2da4360e5cac0a6cc2d
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-05-15 11:02:41 -0700

    menu dropdown in enclosed box

--- src/components/machine/MachineView/index.tsx
+++ src/components/machine/MachineView/index.tsx
@@ -2,10 +2,9 @@ import MachineInputBPM from "../MachineInputBPM";
 import MachineButtonStart from "../MachineButtonStart";
 import MachineButtonNoteAdd from "../MachineButtonNoteAdd/MachineButtonNoteAdd";
 import MachineControlBeats from "../MachineControlBeats";
-import MachineButton from "../MachineButton/MachineButton";
 import { useState, useEffect } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { GridDots } from "tabler-icons-react";
+import { Disclosure, Transition } from "@headlessui/react";
+import { GridDots, X } from "tabler-icons-react";
 
 function MachineView() {
   const [isSticky, setIsSticky] = useState(false);
@@ -30,49 +29,49 @@ function MachineView() {
   const hamburgerMenu = (
     <Disclosure>
       {({ open }) => (
-        <>
-          <Disclosure.Button className="inline-flex items-center justify-center p-2 m-2 rounded-full bg-emerald focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+        <div
+          className={`${
+            open
+              ? "p-2 border rounded-lg absolute top-0 border-black bg-[#0009]"
+              : ""
+          }`}
+        >
+          <Disclosure.Button
+            className={` inline-flex items-center justify-center p-2 m-2 rounded-full bg-emerald focus:outline-none focus:ring-2 focus:ring-inset ${
+              open ? " w-8 h-8" : ""
+            }`}
+          >
             {open ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-              </svg>
+              <X className="w-8 h-8" />
             ) : (
-              <GridDots className="w-8 h-8 text-black justify-self-end" />
+              <GridDots className="w-8 h-8 p-1" />
             )}
           </Disclosure.Button>
-          <Disclosure.Panel className="md:hidden">
-            <div className="flex flex-col items-center pt-4 pb-2 space-y-1">
+          <Disclosure.Panel className="lg:hidden">
+            <div className="xs:flex-col md:flex">
               <MachineControlBeats />
               <MachineInputBPM />
             </div>
           </Disclosure.Panel>
-        </>
+        </div>
       )}
     </Disclosure>
   );
 
   return (
     <nav
-      className={`sticky top-0 z-50 flex items-center  px-4 py-3  ${
+      className={`sticky top-0 z-50 items-center  ${
         isSticky ? "lg:justify-center" : ""
       }`}
     >
-      <div className="h-full">
+      <div className="flex h-full">
         <MachineButtonStart />
         <MachineButtonNoteAdd />
+        <div className="relative flex lg:hidden">
+          {isSticky ? hamburgerMenu : menuItems}
+        </div>
       </div>
-      <div className="lg:hidden">{isSticky ? hamburgerMenu : menuItems}</div>
-      <div className="hidden lg:flex lg:flex-grow">{menuItems}</div>
+      <div className="hidden lg:flex lg:flex-grow ">{menuItems}</div>
     </nav>
   );
 }
